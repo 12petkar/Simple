533f4753acfa1dd13d476744abf9b7e8510f2a7d
Admin <Admin@DESKTOP-7NRUS5E>
2024-03-16 13:16:46 +0530

    updated project

---
+++ src/test/java/Basic/OraganizationsTest.java
@@ -0,0 +1,27 @@
+package Basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OraganizationsTest {
+
+@Test
+
+public void createOraganizationsTest(){
+	
+
+WebDriver driver = new ChromeDriver();
+
+driver.get("http://localhost:8888/");
+
+
+
+}
+
+
+
+
+
+
+}
