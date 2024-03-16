922f047c8ec69d7801077016ac9eee8f2b321c63
Admin <Admin@DESKTOP-7NRUS5E>
2024-03-16 13:38:39 +0530

    added

--- src/test/java/Basic/ContactsTest.java
+++ src/test/java/Basic/ContactsTest.java
@@ -1,5 +1,33 @@
 package Basic;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class ContactsTest {
 
-}
+	@Test
+
+	public void createOraganizationsTest(){
+		
+
+	WebDriver driver = new ChromeDriver();
+
+	driver.get("https://www.ajio.com/");
+	
+
+
+
+	}
+
+
+
+
+
+
+	}
+
+
+
+
+
