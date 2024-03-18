671d7d09fb54a42caf394e91acd893e5c2fc6271
Admin <Admin@DESKTOP-7NRUS5E>
2024-03-18 17:56:54 +0530

    updated

--- src/test/java/Basic/OraganizationsTest.java
+++ src/test/java/Basic/OraganizationsTest.java
@@ -1,27 +1,59 @@
 package Basic;
 
+import java.io.IOException;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+
+
+import CommonUtils.PropertyFileUtil;
+import CommonUtils.WebDriverUtil;
+
 public class OraganizationsTest {
 
-@Test
 
-public void createOraganizationsTest(){
+	PropertyFileUtil putil = new  PropertyFileUtil();
 	
+	 WebDriverUtil wutil = new  WebDriverUtil();
 
-WebDriver driver = new ChromeDriver();
+	 @Test
+	 
+	 public void OraganizationTest() throws IOException, InterruptedException {
+	 
+ WebDriver driver = new ChromeDriver();
+   
+   wutil.maximize(driver); //To Maxmize the window
+   wutil.implicitwait(driver);//To apply wait
 
-driver.get("http://localhost:8888/");
+   
+   //To Read the data property File
+  String URL = putil.getDataPropertyFile("Url");
+  
+ String USERNAME = putil.getDataPropertyFile("Username");
+  String PASSWORD = putil.getDataPropertyFile("Password");  
+	 
+	 driver.get(URL); //To Launch the application
+	 
+	 //To Login Application
+	driver.findElement(By.name("user_name")).sendKeys(USERNAME);
+	Thread.sleep(2000);
+	driver.findElement(By.name("user_password")).sendKeys(PASSWORD);
+	Thread.sleep(2000);
+	 
+
+	
+	 
+	
+	
+	
+	
+	driver.findElement(By.id("submitButton")).click();
+    Thread.sleep(2000);
 
 
 
-}
-
-
-
-
-
-
+	 }
 }
